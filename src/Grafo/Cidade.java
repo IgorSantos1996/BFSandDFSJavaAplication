d9bc07581ce5c9947fe715075ff3008de856f42d
igorb22 <igorb22@live.com>
2019-06-10 22:11:46 -0300

    adicionando metodo generico que faz as cidades da rota piscarem

--- src/Grafo/Cidade.java
+++ src/Grafo/Cidade.java
@@ -8,16 +8,25 @@ public class Cidade {
     private String nome;
     private boolean visitado;
     private List<Adjacente> adjacenteList;
+    private int x; // coordenadas do layout
+    private int y; 	
 
-    public Cidade(String nome) {
+
+     public Cidade(String nome,int x,int y) {
         this.nome = nome;
+        this.x = x;
+        this.y = y;
         visitado = false;
         adjacenteList = new ArrayList<Adjacente>();
-
     }
 
     public Cidade() {
+    }
 
+    public Cidade(String nome2) {
+    	this.nome = nome2;
+        visitado = false;
+        adjacenteList = new ArrayList<Adjacente>();
     }
 
     public void addCidadeAdjacente(Adjacente cidade) {
@@ -28,7 +37,26 @@ public class Cidade {
         adjacenteList.add(cidade);
     }
 
-    public String getNome() {
+    
+    
+    
+    public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public String getNome() {
         return nome;
     }
 
